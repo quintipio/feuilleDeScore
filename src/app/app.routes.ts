2eeb6f4583a83d3b330fa764d3e620e4fcef98dc
DELFOUR Quentin <quentin.delfour@orange.com>
2024-10-16 14:56:23 +0200

    Ajout des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { GamesComponent } from './games/games.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'users',
+    title: 'Joueurs',
+    component: UsersComponent,
+  },
+  {
+    path: 'games',
+    title: 'Jeux',
+    component: GamesComponent,
+  },
+];
