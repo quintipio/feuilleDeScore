cbe5cdca5a9da92293ea3823cda9c5a414258d6e
DELFOUR Quentin <quentin.delfour@orange.com>
2024-10-23 11:30:11 +0200

    Création du composant d'input de valeur numérique

---
+++ src/app/components/input-score/input-score.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-score',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './input-score.component.html',
+  styleUrl: './input-score.component.css'
+})
+export class InputScoreComponent {
+  value: number = 0;
+
+  @Output() valueChange: EventEmitter<number> = new EventEmitter<number>();
+
+  decrement() {
+    this.value--;
+    this.valueChange.emit(this.value);
+  }
+
+  increment() {
+    this.value++;
+    this.valueChange.emit(this.value);
+  }
+
+  onInputChange(event: any) {
+    const inputValue = event.target.value;
+    if (!isNaN(inputValue) && inputValue !== '') {
+      this.value = parseInt(inputValue, 10);
+    } else {
+      this.value = 0;
+    }
+    this.valueChange.emit(this.value);
+  }
+}
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,21 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../service/users.service';
 import { User } from '../models/user.model';
+import {InputScoreComponent} from '../components/input-score/input-score.component'
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [InputScoreComponent],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css'
 })
 export class GamesComponent {
   selectedUsers: User[] = [];
+  parentValue: number = 0;
 
   userService = inject(UserService);
 
   ngOnInit(): void {
     this.selectedUsers = this.userService.getSelectedUsers();
+  }
+
+  onValueChange(newValue: number) {
+    this.parentValue = newValue;
+    console.log('Nouvelle valeur reçue du composant enfant:', newValue);
   }
 
 }
