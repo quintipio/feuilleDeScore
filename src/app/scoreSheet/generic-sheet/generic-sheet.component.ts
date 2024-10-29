c166a90e138efa82500c011f1ffd760b40233e11
DELFOUR Quentin <quentin.delfour@orange.com>
2024-10-29 16:37:11 +0100

    Création du tableau de score

--- src/app/scoreSheet/generic-sheet/generic-sheet.component.ts
+++ src/app/scoreSheet/generic-sheet/generic-sheet.component.ts
@@ -2,18 +2,40 @@ import { Component, inject } from '@angular/core';
 import { Table } from '../../models/table.model';
 import { TableService } from '../../service/table.service';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../service/users.service';
+import { User } from '../../models/user.model';
+import { CommonModule } from '@angular/common';
+
+type UserColumn = {
+  position: number;
+  user: User;
+}
+
+type CountRoundRow = {
+  user:UserColumn
+  value: number;
+}
+
+type RoundRow = {
+  row: number;
+  points: CountRoundRow[];
+}
 
 @Component({
   selector: 'app-generic-sheet',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './generic-sheet.component.html',
   styleUrl: './generic-sheet.component.css'
 })
 export class GenericSheetComponent {
 
   private tableService = inject(TableService);
-  table : Table | undefined;
+  private userService = inject(UserService);
+
+  table: Table | undefined;
+  round: RoundRow[] = [];
+  users: UserColumn[] = [];
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
@@ -23,9 +45,66 @@ export class GenericSheetComponent {
       if (idTable) {
         this.tableService.getTable(idTable).subscribe(table => {
           this.table = table;
+          if (this.table) {
+            this.userService.getUsersById(this.table.usersId).subscribe((users: User[]) => {
+              if (this.table) {
+                this.table.users = users;
+                this.generateUsers(this.table.users);
+                this.addRound();
+              }
+            });
+          }
         });
       }
     });
   }
+
+  generateUsers(usersToAdd: User[]) {
+    var i = 1;
+    usersToAdd.forEach((user) => {
+      const column: UserColumn = {
+        "position": i,
+        "user": user
+      }
+      this.users.push(column);
+      i += 1;
+    });
+    this.users.sort((a, b) => a.position - b.position);
+  }
+
+  trackByUserPosition(index: number, user: UserColumn): number {
+    return user.position;
+  }
+
+  getSum(user: UserColumn): number {
+    let totalValue = 0;
+    this.round.forEach(roundRow => {
+      roundRow.points.forEach(countRoundRow => {
+        if (countRoundRow.user.position === user.position) {
+          totalValue += countRoundRow.value;
+        }
+      });
+    });
+
+    return totalValue;
+  }
+
+
+  addRound() {
+    const row: RoundRow = {
+      "row" : this.round.length + 1,
+      "points" :[],
+    }
+    this.users.forEach((userRow) => {
+      const count: CountRoundRow=  {
+        user : userRow,
+        value: 3
+      }
+      row.points.push(count);
+    })
+    this.round.push(row);
+    this.round.sort((a, b) => a.row - b.row);
+  }
+
 
 }
