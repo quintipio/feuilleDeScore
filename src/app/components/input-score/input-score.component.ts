bf5a86538f42cdc13189dbf55b8a40c35e3b20c8
DELFOUR Quentin <quentin.delfour@orange.com>
2026-02-04 14:39:34 +0100

    Modification de input score pour des values personalisées

--- src/app/components/input-score/input-score.component.ts
+++ src/app/components/input-score/input-score.component.ts
@@ -1,58 +1,90 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input-score',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './input-score.component.html',
   styleUrl: './input-score.component.css'
 })
-export class InputScoreComponent {
+export class InputScoreComponent<T extends string | number = number> implements OnInit {
   @Input() name: string = '';
-  @Input() initValue : number | undefined;
-  @Input() min : number | undefined;
-  @Input() max : number | undefined;
-  @Input() inc : number | undefined;
-  value: number = 0;
+  @Input() initValue: T | undefined;
+  @Input() min: number | undefined;
+  @Input() max: number | undefined;
+  @Input() inc: number = 1;
+  @Input() values: T[] | undefined;
 
-  @Output() valueChange: EventEmitter<number> = new EventEmitter<number>();
+  value!: T;
 
-  ngOnInit(){
-    if(this.initValue !== undefined){
+  @Output() valueChange = new EventEmitter<T>();
+
+  ngOnInit() {
+    if (this.initValue !== undefined) {
       this.value = this.initValue;
+    }
+    else if (this.values && this.values.length > 0) {
+      this.value = this.values[0];
+    }
+    else {
+      this.value = 0 as unknown as T;
     }
   }
 
   decrement() {
-    if(this.inc == undefined){
-      this.inc = 1;
-    }
-    if(this.min == undefined || this.value-1 >= this.min){
-      this.value = this.value-this.inc;
-      this.valueChange.emit(this.value);
+    if (this.values && this.values.length > 0) {
+      const currentIndex = this.values.indexOf(this.value);
+      if (currentIndex > 0) {
+        this.updateValue(this.values[currentIndex - 1]);
+      }
+    } else if (typeof this.value === 'number') {
+      const step = this.inc ?? 1;
+      if (this.min === undefined || this.value - step >= this.min) {
+        this.updateValue((this.value - step) as unknown as T);
+      }
     }
   }
 
   increment() {
-    if(this.inc == undefined){
-      this.inc = 1;
+    if (this.values && this.values.length > 0) {
+      const currentIndex = this.values.indexOf(this.value);
+      if (currentIndex < this.values.length - 1) {
+        this.updateValue(this.values[currentIndex + 1]);
+      }
+    } else if (typeof this.value === 'number') {
+      const step = this.inc ?? 1;
+      if (this.max === undefined || this.value + step <= this.max) {
+        this.updateValue((this.value + step) as unknown as T);
+      }
     }
-    if(this.max == undefined || this.value+1 <= this.max){
-      this.value = this.value+this.inc;
-      this.valueChange.emit(this.value);
+  }
+
+  private updateValue(newValue: T) {
+    this.value = newValue;
+    this.valueChange.emit(this.value);
+  }
+
+  disabledMin(): boolean {
+    if (this.values && this.values.length > 0) {
+      return this.value === this.values[0];
     }
+    return (typeof this.value === 'number' && this.min !== undefined)
+      ? this.value <= this.min
+      : false;
   }
 
-  disabledMin() {
-    return (this.min !== undefined)?this.value <= this.min:false;
+  disabledMax(): boolean {
+    if (this.values && this.values.length > 0) {
+      return this.value === this.values[this.values.length - 1];
+    }
+    return (typeof this.value === 'number' && this.max !== undefined)
+      ? this.value >= this.max
+      : false;
   }
 
-  disabledMax() {
-    return (this.max !== undefined)?this.value >= this.max:false;
-  }
-
-  reinit(value : number){
+  reinit(value: T) {
     this.value = value;
   }
 }
